1c97ded5a7248b7ff9e7ad295fbe749b59eb8f1b
ihatelethargy <naturalflavor1007@naver.com>
2020-07-01 16:07:22 +0900

    slider;

---
+++ carousel/js/index.js
@@ -0,0 +1,20 @@
+const SHOWING_CLASS = "showing";
+const firstSlide = document.querySelector(".slider__item:first-child");
+
+function slide(){
+    const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+    console.log(currentSlide);
+    if(currentSlide){
+        currentSlide.classList.remove(SHOWING_CLASS);
+        const nextSlide = currentSlide.nextElementSibling;
+        if(nextSlide){
+            nextSlide.classList.add(SHOWING_CLASS);
+        }else{
+            firstSlide.classList.add(SHOWING_CLASS);    
+        }
+    }else{
+        firstSlide.classList.add(SHOWING_CLASS);
+    }
+}
+slide();
+setInterval(slide, 2000);
