2bc623b0ecbf571c3df9ec2b7c2dd757701bb234
ihatelethargy <naturalflavor1007@naver.com>
2020-09-08 12:37:05 +0900

    detail page final update

---
+++ medi/detail/detailapp_three_3.js
@@ -0,0 +1,67 @@
+// Navigate product images 
+// product 1
+const BigImage = document.getElementById('big-image');
+const imgSlider = document.getElementById('img-slider');
+
+imgSlider.addEventListener('click', event => {
+
+    if (event.target === product_1) {
+         BigImage.setAttribute ("src","images/product3/product3_3_1.PNG");
+    }
+
+    else if (event.target === product_2) {
+        BigImage.setAttribute ("src","images/product3/product3_3_2.PNG");
+    }
+
+    else {
+    BigImage.setAttribute ("src","images/product3/product3_3_3.PNG");
+    }
+});
+
+
+// Navigate information 
+
+const chooseInfo = document.getElementById('more-infos');
+const choose = document.getElementsByClassName('choose');
+const paragraph = document.getElementsByClassName('paragraph');
+
+
+function styleItem (a,b,c) {
+    a.style.cssText = 'color:black ; border-bottom: 2px solid black ; padding-bottom: 6px';
+    b.style.cssText = 'color:gray ; border-bottom: none';
+    c.style.cssText = 'color:gray ; border-bottom: none';
+}
+
+function displayPrph (e,f,g) {
+        e.style.display = 'block';
+        f.style.display = 'none';
+        g.style.display = 'none';
+}
+
+chooseInfo.addEventListener('click', event => {
+   
+       if (event.target === choose[0]) {    
+           
+                 styleItem (choose[0],choose[1],choose[2])
+                 displayPrph (paragraph[0],paragraph[2],paragraph[1])
+        }  
+
+       else if (event.target === choose[1]) {  
+
+                 styleItem (choose[1],choose[0],choose[2])
+                 displayPrph (paragraph[1],paragraph[0],paragraph[2])
+        }  
+
+       else   {
+                  styleItem (choose[2],choose[0],choose[1])
+                  displayPrph (paragraph[2],paragraph[0],paragraph[1])
+        }     
+});
+
+// add items to the cart
+
+const addToCart = document.getElementById('add-to-cart');
+const itemsAdded = document.getElementById('items-added');
+const counter = document.getElementById('counter');
+
+addToCart.addEventListener('click',ev => itemsAdded.textContent = (counter.value));
